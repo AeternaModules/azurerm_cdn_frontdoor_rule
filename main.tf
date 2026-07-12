820815c9009265d053139a00f38a0543518a7af6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 13:19:04 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
 
   actions {
     dynamic "request_header_action" {
-      for_each = each.value.actions.request_header_action != null ? [each.value.actions.request_header_action] : []
+      for_each = each.value.actions.request_header_action != null ? each.value.actions.request_header_action : []
       content {
         header_action = request_header_action.value.header_action
         header_name   = request_header_action.value.header_name
@@ -16,7 +16,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
       }
     }
     dynamic "response_header_action" {
-      for_each = each.value.actions.response_header_action != null ? [each.value.actions.response_header_action] : []
+      for_each = each.value.actions.response_header_action != null ? each.value.actions.response_header_action : []
       content {
         header_action = response_header_action.value.header_action
         header_name   = response_header_action.value.header_name
@@ -60,7 +60,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
     for_each = each.value.conditions != null ? [each.value.conditions] : []
     content {
       dynamic "client_port_condition" {
-        for_each = conditions.value.client_port_condition != null ? [conditions.value.client_port_condition] : []
+        for_each = conditions.value.client_port_condition != null ? conditions.value.client_port_condition : []
         content {
           match_values     = client_port_condition.value.match_values
           negate_condition = client_port_condition.value.negate_condition
@@ -68,7 +68,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "cookies_condition" {
-        for_each = conditions.value.cookies_condition != null ? [conditions.value.cookies_condition] : []
+        for_each = conditions.value.cookies_condition != null ? conditions.value.cookies_condition : []
         content {
           cookie_name      = cookies_condition.value.cookie_name
           match_values     = cookies_condition.value.match_values
@@ -78,7 +78,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "host_name_condition" {
-        for_each = conditions.value.host_name_condition != null ? [conditions.value.host_name_condition] : []
+        for_each = conditions.value.host_name_condition != null ? conditions.value.host_name_condition : []
         content {
           match_values     = host_name_condition.value.match_values
           negate_condition = host_name_condition.value.negate_condition
@@ -87,7 +87,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "http_version_condition" {
-        for_each = conditions.value.http_version_condition != null ? [conditions.value.http_version_condition] : []
+        for_each = conditions.value.http_version_condition != null ? conditions.value.http_version_condition : []
         content {
           match_values     = http_version_condition.value.match_values
           negate_condition = http_version_condition.value.negate_condition
@@ -95,7 +95,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "is_device_condition" {
-        for_each = conditions.value.is_device_condition != null ? [conditions.value.is_device_condition] : []
+        for_each = conditions.value.is_device_condition != null ? conditions.value.is_device_condition : []
         content {
           match_values     = is_device_condition.value.match_values
           negate_condition = is_device_condition.value.negate_condition
@@ -103,7 +103,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "post_args_condition" {
-        for_each = conditions.value.post_args_condition != null ? [conditions.value.post_args_condition] : []
+        for_each = conditions.value.post_args_condition != null ? conditions.value.post_args_condition : []
         content {
           match_values     = post_args_condition.value.match_values
           negate_condition = post_args_condition.value.negate_condition
@@ -113,7 +113,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "query_string_condition" {
-        for_each = conditions.value.query_string_condition != null ? [conditions.value.query_string_condition] : []
+        for_each = conditions.value.query_string_condition != null ? conditions.value.query_string_condition : []
         content {
           match_values     = query_string_condition.value.match_values
           negate_condition = query_string_condition.value.negate_condition
@@ -122,7 +122,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "remote_address_condition" {
-        for_each = conditions.value.remote_address_condition != null ? [conditions.value.remote_address_condition] : []
+        for_each = conditions.value.remote_address_condition != null ? conditions.value.remote_address_condition : []
         content {
           match_values     = remote_address_condition.value.match_values
           negate_condition = remote_address_condition.value.negate_condition
@@ -130,7 +130,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "request_body_condition" {
-        for_each = conditions.value.request_body_condition != null ? [conditions.value.request_body_condition] : []
+        for_each = conditions.value.request_body_condition != null ? conditions.value.request_body_condition : []
         content {
           match_values     = request_body_condition.value.match_values
           negate_condition = request_body_condition.value.negate_condition
@@ -139,7 +139,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "request_header_condition" {
-        for_each = conditions.value.request_header_condition != null ? [conditions.value.request_header_condition] : []
+        for_each = conditions.value.request_header_condition != null ? conditions.value.request_header_condition : []
         content {
           header_name      = request_header_condition.value.header_name
           match_values     = request_header_condition.value.match_values
@@ -149,7 +149,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "request_method_condition" {
-        for_each = conditions.value.request_method_condition != null ? [conditions.value.request_method_condition] : []
+        for_each = conditions.value.request_method_condition != null ? conditions.value.request_method_condition : []
         content {
           match_values     = request_method_condition.value.match_values
           negate_condition = request_method_condition.value.negate_condition
@@ -157,7 +157,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "request_scheme_condition" {
-        for_each = conditions.value.request_scheme_condition != null ? [conditions.value.request_scheme_condition] : []
+        for_each = conditions.value.request_scheme_condition != null ? conditions.value.request_scheme_condition : []
         content {
           match_values     = request_scheme_condition.value.match_values
           negate_condition = request_scheme_condition.value.negate_condition
@@ -165,7 +165,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "request_uri_condition" {
-        for_each = conditions.value.request_uri_condition != null ? [conditions.value.request_uri_condition] : []
+        for_each = conditions.value.request_uri_condition != null ? conditions.value.request_uri_condition : []
         content {
           match_values     = request_uri_condition.value.match_values
           negate_condition = request_uri_condition.value.negate_condition
@@ -174,7 +174,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "server_port_condition" {
-        for_each = conditions.value.server_port_condition != null ? [conditions.value.server_port_condition] : []
+        for_each = conditions.value.server_port_condition != null ? conditions.value.server_port_condition : []
         content {
           match_values     = server_port_condition.value.match_values
           negate_condition = server_port_condition.value.negate_condition
@@ -182,7 +182,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "socket_address_condition" {
-        for_each = conditions.value.socket_address_condition != null ? [conditions.value.socket_address_condition] : []
+        for_each = conditions.value.socket_address_condition != null ? conditions.value.socket_address_condition : []
         content {
           match_values     = socket_address_condition.value.match_values
           negate_condition = socket_address_condition.value.negate_condition
@@ -190,7 +190,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "ssl_protocol_condition" {
-        for_each = conditions.value.ssl_protocol_condition != null ? [conditions.value.ssl_protocol_condition] : []
+        for_each = conditions.value.ssl_protocol_condition != null ? conditions.value.ssl_protocol_condition : []
         content {
           match_values     = ssl_protocol_condition.value.match_values
           negate_condition = ssl_protocol_condition.value.negate_condition
@@ -198,7 +198,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "url_file_extension_condition" {
-        for_each = conditions.value.url_file_extension_condition != null ? [conditions.value.url_file_extension_condition] : []
+        for_each = conditions.value.url_file_extension_condition != null ? conditions.value.url_file_extension_condition : []
         content {
           match_values     = url_file_extension_condition.value.match_values
           negate_condition = url_file_extension_condition.value.negate_condition
@@ -207,7 +207,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "url_filename_condition" {
-        for_each = conditions.value.url_filename_condition != null ? [conditions.value.url_filename_condition] : []
+        for_each = conditions.value.url_filename_condition != null ? conditions.value.url_filename_condition : []
         content {
           match_values     = url_filename_condition.value.match_values
           negate_condition = url_filename_condition.value.negate_condition
@@ -216,7 +216,7 @@ resource "azurerm_cdn_frontdoor_rule" "cdn_frontdoor_rules" {
         }
       }
       dynamic "url_path_condition" {
-        for_each = conditions.value.url_path_condition != null ? [conditions.value.url_path_condition] : []
+        for_each = conditions.value.url_path_condition != null ? conditions.value.url_path_condition : []
         content {
           match_values     = url_path_condition.value.match_values
           negate_condition = url_path_condition.value.negate_condition
--- variables.tf
+++ variables.tf
@@ -133,16 +133,16 @@ EOT
     order                     = number
     behavior_on_match         = optional(string) # Default: "Continue"
     actions = object({
-      request_header_action = optional(object({
+      request_header_action = optional(list(object({
         header_action = string
         header_name   = string
         value         = optional(string)
-      }))
-      response_header_action = optional(object({
+      })))
+      response_header_action = optional(list(object({
         header_action = string
         header_name   = string
         value         = optional(string)
-      }))
+      })))
       route_configuration_override_action = optional(object({
         cache_behavior                = optional(string)
         cache_duration                = optional(string)
@@ -167,212 +167,116 @@ EOT
       }))
     })
     conditions = optional(object({
-      client_port_condition = optional(object({
+      client_port_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
-      }))
-      cookies_condition = optional(object({
+      })))
+      cookies_condition = optional(list(object({
         cookie_name      = string
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      host_name_condition = optional(object({
+      })))
+      host_name_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      http_version_condition = optional(object({
+      })))
+      http_version_condition = optional(list(object({
         match_values     = set(string)
         negate_condition = optional(bool)   # Default: false
         operator         = optional(string) # Default: "Equal"
-      }))
-      is_device_condition = optional(object({
+      })))
+      is_device_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool)   # Default: false
         operator         = optional(string) # Default: "Equal"
-      }))
-      post_args_condition = optional(object({
+      })))
+      post_args_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         post_args_name   = string
         transforms       = optional(set(string))
-      }))
-      query_string_condition = optional(object({
+      })))
+      query_string_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      remote_address_condition = optional(object({
+      })))
+      remote_address_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool)   # Default: false
         operator         = optional(string) # Default: "IPMatch"
-      }))
-      request_body_condition = optional(object({
+      })))
+      request_body_condition = optional(list(object({
         match_values     = list(string)
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      request_header_condition = optional(object({
+      })))
+      request_header_condition = optional(list(object({
         header_name      = string
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      request_method_condition = optional(object({
+      })))
+      request_method_condition = optional(list(object({
         match_values     = set(string)
         negate_condition = optional(bool)   # Default: false
         operator         = optional(string) # Default: "Equal"
-      }))
-      request_scheme_condition = optional(object({
+      })))
+      request_scheme_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool)   # Default: false
         operator         = optional(string) # Default: "Equal"
-      }))
-      request_uri_condition = optional(object({
+      })))
+      request_uri_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      server_port_condition = optional(object({
+      })))
+      server_port_condition = optional(list(object({
         match_values     = set(string)
         negate_condition = optional(bool) # Default: false
         operator         = string
-      }))
-      socket_address_condition = optional(object({
+      })))
+      socket_address_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool)   # Default: false
         operator         = optional(string) # Default: "IPMatch"
-      }))
-      ssl_protocol_condition = optional(object({
+      })))
+      ssl_protocol_condition = optional(list(object({
         match_values     = set(string)
         negate_condition = optional(bool)   # Default: false
         operator         = optional(string) # Default: "Equal"
-      }))
-      url_file_extension_condition = optional(object({
+      })))
+      url_file_extension_condition = optional(list(object({
         match_values     = list(string)
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      url_filename_condition = optional(object({
+      })))
+      url_filename_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
-      url_path_condition = optional(object({
+      })))
+      url_path_condition = optional(list(object({
         match_values     = optional(list(string))
         negate_condition = optional(bool) # Default: false
         operator         = string
         transforms       = optional(set(string))
-      }))
+      })))
     }))
   }))
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.order >= 0
-      )
-    ])
-    error_message = "must be at least 0"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.url_redirect_action == null || (length(v.actions.url_redirect_action.destination_hostname) >= 0 && length(v.actions.url_redirect_action.destination_hostname) <= 2048)
-      )
-    ])
-    error_message = "must be between 0 and 2048 characters"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.url_redirect_action == null || (v.actions.url_redirect_action.destination_fragment == null || (length(v.actions.url_redirect_action.destination_fragment) >= 0 && length(v.actions.url_redirect_action.destination_fragment) <= 1024))
-      )
-    ])
-    error_message = "must be between 0 and 1024 characters"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.url_rewrite_action == null || (length(v.actions.url_rewrite_action.source_pattern) > 0)
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.url_rewrite_action == null || (length(v.actions.url_rewrite_action.destination) > 0)
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.request_header_action == null || (length(v.actions.request_header_action.header_name) > 0)
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.request_header_action == null || (v.actions.request_header_action.value == null || (length(v.actions.request_header_action.value) > 0))
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.response_header_action == null || (length(v.actions.response_header_action.header_name) > 0)
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.actions.response_header_action == null || (v.actions.response_header_action.value == null || (length(v.actions.response_header_action.value) > 0))
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.conditions == null || (v.conditions.post_args_condition == null || (length(v.conditions.post_args_condition.post_args_name) > 0))
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.conditions == null || (v.conditions.request_header_condition == null || (length(v.conditions.request_header_condition.header_name) > 0))
-      )
-    ])
-    error_message = "must not be empty"
-  }
-  validation {
-    condition = alltrue([
-      for k, v in var.cdn_frontdoor_rules : (
-        v.conditions == null || (v.conditions.cookies_condition == null || (length(v.conditions.cookies_condition.cookie_name) > 0))
-      )
-    ])
-    error_message = "must not be empty"
-  }
   # --- Unconfirmed validation candidates, derived from azurerm_cdn_frontdoor_rule's provider source ---
   # Not auto-enabled: either a bespoke provider validator we can't safely translate,
   # or a path that crosses a list-typed block (needs its own for_each wrapping).
@@ -385,18 +289,45 @@ EOT
   #   source:    [from validate.FrontDoorRuleSetID] err != nil
   # path: behavior_on_match
   #   source:    validation.StringInSlice value list is not a literal []string - likely a generated PossibleValuesFor*() helper; resolve separately
+  # path: order
+  #   condition: value >= 0
+  #   message:   must be at least 0
   # path: actions.url_redirect_action.redirect_type
   #   source:    validation.StringInSlice value list is not a literal []string - likely a generated PossibleValuesFor*() helper; resolve separately
   # path: actions.url_redirect_action.redirect_protocol
   #   source:    validation.StringInSlice value list is not a literal []string - likely a generated PossibleValuesFor*() helper; resolve separately
   # path: actions.url_redirect_action.destination_path
   #   source:    validate.CdnFrontDoorUrlRedirectActionDestinationPath: no recognizable `if ... { errors = append(...) }` pattern - read it by hand
+  # path: actions.url_redirect_action.destination_hostname
+  #   condition: length(value) >= 0 && length(value) <= 2048
+  #   message:   must be between 0 and 2048 characters
   # path: actions.url_redirect_action.query_string
   #   source:    validate.CdnFrontDoorUrlRedirectActionQueryString: no recognizable `if ... { errors = append(...) }` pattern - read it by hand
+  # path: actions.url_redirect_action.destination_fragment
+  #   condition: length(value) >= 0 && length(value) <= 1024
+  #   message:   must be between 0 and 1024 characters
+  # path: actions.url_rewrite_action.source_pattern
+  #   condition: length(value) > 0
+  #   message:   must not be empty
+  # path: actions.url_rewrite_action.destination
+  #   condition: length(value) > 0
+  #   message:   must not be empty
   # path: actions.request_header_action.header_action
   #   source:    validation.StringInSlice value list is not a literal []string - likely a generated PossibleValuesFor*() helper; resolve separately
+  # path: actions.request_header_action.header_name
+  #   condition: length(value) > 0
+  #   message:   must not be empty
+  # path: actions.request_header_action.value
+  #   condition: length(value) > 0
+  #   message:   must not be empty
   # path: actions.response_header_action.header_action
   #   source:    validation.StringInSlice value list is not a literal []string - likely a generated PossibleValuesFor*() helper; resolve separately
+  # path: actions.response_header_action.header_name
+  #   condition: length(value) > 0
+  #   message:   must not be empty
+  # path: actions.response_header_action.value
+  #   condition: length(value) > 0
+  #   message:   must not be empty
   # path: actions.route_configuration_override_action.cdn_frontdoor_origin_group_id
   #   source:    [from validate.FrontDoorOriginGroupID] !ok
   # path: actions.route_configuration_override_action.cdn_frontdoor_origin_group_id
@@ -409,5 +340,14 @@ EOT
   #   source:    validation.StringInSlice value list is not a literal []string - likely a generated PossibleValuesFor*() helper; resolve separately
   # path: actions.route_configuration_override_action.cache_duration
   #   source:    validate.CdnFrontDoorCacheDuration: no recognizable `if ... { errors = append(...) }` pattern - read it by hand
+  # path: conditions.post_args_condition.post_args_name
+  #   condition: length(value) > 0
+  #   message:   must not be empty
+  # path: conditions.request_header_condition.header_name
+  #   condition: length(value) > 0
+  #   message:   must not be empty
+  # path: conditions.cookies_condition.cookie_name
+  #   condition: length(value) > 0
+  #   message:   must not be empty
 }
 
