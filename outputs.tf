c721989f5c3ad75258b47be3843696eb419445bd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "cdn_frontdoor_rules" {
-  description = "All cdn_frontdoor_rule resources"
-  value       = azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules
-}
 output "cdn_frontdoor_rules_actions" {
-  description = "List of actions values across all cdn_frontdoor_rules"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : v.actions]
+  description = "Map of actions values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.actions }
 }
 output "cdn_frontdoor_rules_behavior_on_match" {
-  description = "List of behavior_on_match values across all cdn_frontdoor_rules"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : v.behavior_on_match]
+  description = "Map of behavior_on_match values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.behavior_on_match }
 }
 output "cdn_frontdoor_rules_cdn_frontdoor_rule_set_id" {
-  description = "List of cdn_frontdoor_rule_set_id values across all cdn_frontdoor_rules"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : v.cdn_frontdoor_rule_set_id]
+  description = "Map of cdn_frontdoor_rule_set_id values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.cdn_frontdoor_rule_set_id }
 }
 output "cdn_frontdoor_rules_cdn_frontdoor_rule_set_name" {
-  description = "List of cdn_frontdoor_rule_set_name values across all cdn_frontdoor_rules"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : v.cdn_frontdoor_rule_set_name]
+  description = "Map of cdn_frontdoor_rule_set_name values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.cdn_frontdoor_rule_set_name }
 }
 output "cdn_frontdoor_rules_conditions" {
-  description = "List of conditions values across all cdn_frontdoor_rules"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : v.conditions]
+  description = "Map of conditions values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.conditions }
 }
 output "cdn_frontdoor_rules_name" {
-  description = "List of name values across all cdn_frontdoor_rules"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : v.name]
+  description = "Map of name values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.name }
 }
 output "cdn_frontdoor_rules_order" {
-  description = "List of order values across all cdn_frontdoor_rules"
-  value       = [for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : v.order]
+  description = "Map of order values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.order }
 }
 
