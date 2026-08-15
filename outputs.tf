290fa226b3a39ad580f2f2d4115d7b095e4bfab2
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:02:29 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "cdn_frontdoor_rules_id" {
 }
 output "cdn_frontdoor_rules_actions" {
   description = "Map of actions values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
-  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.actions if v.actions != null && length(v.actions) > 0 }
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => one(v.actions) if v.actions != null && length(v.actions) > 0 }
 }
 output "cdn_frontdoor_rules_behavior_on_match" {
   description = "Map of behavior_on_match values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
@@ -20,7 +20,7 @@ output "cdn_frontdoor_rules_cdn_frontdoor_rule_set_name" {
 }
 output "cdn_frontdoor_rules_conditions" {
   description = "Map of conditions values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
-  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => v.conditions if v.conditions != null && length(v.conditions) > 0 }
+  value       = { for k, v in azurerm_cdn_frontdoor_rule.cdn_frontdoor_rules : k => one(v.conditions) if v.conditions != null && length(v.conditions) > 0 }
 }
 output "cdn_frontdoor_rules_name" {
   description = "Map of name values across all cdn_frontdoor_rules, keyed the same as var.cdn_frontdoor_rules"
